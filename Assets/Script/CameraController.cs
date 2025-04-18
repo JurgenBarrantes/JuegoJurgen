92eb0ef2104978af871bc61372261c9965c909e7
JurgenBarrantes <jurgennekbh@gmail.com>
2025-04-18 12:18:43 -0500

    Semana 04

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.Find("NinjaPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float positionX = player.transform.position.x -2f;
+        float positionY = player.transform.position.y -2f;
+        float positionZ = transform.position.z;
+        transform.position = new Vector3(positionX, positionY, positionZ);
+    }
+}
