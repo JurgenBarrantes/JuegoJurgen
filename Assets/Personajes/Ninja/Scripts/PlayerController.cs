ba412c8edc8eb371003f3daa484f8a61376ccb01
JurgenBarrantes <jurgennekbh@gmail.com>
2025-04-25 14:41:07 -0500

    Semana 05

--- Assets/Personajes/Ninja/Scripts/PlayerController.cs
+++ Assets/Personajes/Ninja/Scripts/PlayerController.cs
@@ -6,18 +6,53 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer sr;
     Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool puedeMoverseVerticalMente = false;
+    private float defaultGravityScale = 1f;
+    private bool puedeSaltar = true;
     void Start()
     {
+        Debug.Log("INICIANDO PLAYER CONTROLLER");
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        defaultGravityScale = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         SetupMoverseHorizontal();
         SetupSalto();
+        SetupMoverVertical();
+    }
+
+    //coliciones sem05
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo")){
+            MaleZombieController zombie = collision.gameObject.GetComponent<MaleZombieController>();
+            Debug.Log($"Colision con el enemigo: {zombie.puntosVida}");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log($"Triggera con: {other.gameObject.name}");
+        if(other.gameObject.name == "Muro"){
+            puedeMoverseVerticalMente= true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"Trigger con: {other.gameObject.name}");
+        if (other.gameObject.name == "Muro") {
+            puedeMoverseVerticalMente = false;
+            rb.gravityScale = defaultGravityScale;
+        }
     }
 
     void SetupMoverseHorizontal()
@@ -38,11 +73,30 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+    void SetupMoverVertical()
+    {
+        if(!puedeMoverseVerticalMente) return;
+        
+        rb.gravityScale = 0;
+        rb.linearVelocityY = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.linearVelocityY = 10;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+                rb.linearVelocityY = -10;
+        }
+        
+    }
     void SetupSalto()
     {
+        if (!puedeSaltar) return;
         if (Input.GetKeyUp(KeyCode.Space))
         {
             rb.linearVelocityY = 12.5f;
         }
     }
+
+    
 }
